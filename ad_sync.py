becff1cc67dd007fadf20fb155c7a7e99e7fb05e
AlanAraujo72 <39307204+AlanAraujo72@users.noreply.github.com>
2022-10-29 18:06:09 -0300

    Update ad_sync.py

--- ad_sync.py
+++ ad_sync.py
@@ -8,11 +8,11 @@ from pyad import *
 
 #import pyad.adquery
 
-#with io.open("D:/Users/alves.STJ/PycharmProjects/AD-SYNC/data_stj2.csv", "r", encoding="utf-8") as f1:
+#with io.open("data.csv", "r", encoding="utf-8") as f1:
  #   data = f1.read()
  #   f1.close()
 #file_row = data.split("\n")
-file_row = pd.read_csv("D:/Users/alves.STJ/PycharmProjects/AD-SYNC/data_stj2.csv", delimiter=";")
+file_row = pd.read_csv("data.csv", delimiter=";")
 #ou=pyad.adcontainer.ADContainer.from_dn("OU=STI,OU=ST,OU=Usuarios,OU=AAPADM,OU=Usr e Cmp W2000,OU=Objetos do STJ,DC=STJ,DC=GOV,DC=BR")
 
 q = adquery.ADQuery()
@@ -20,11 +20,11 @@ q = adquery.ADQuery()
 q.execute_query(
     attributes=["userprincipalName","givenName","sn","distinguishedName"],
     where_clause = "objectClass = 'user'",
-    base_dn="OU=STI,OU=ST,OU=Usuarios,OU=AAPADM,OU=Usr e Cmp W2000,OU=Objetos do STJ,DC=STJ,DC=GOV,DC=BR"
+    base_dn="caminho ou"
 )
 
-pyad.set_defaults(ldap_server="sfwp-ad-01.stj.gov.br",
-username="orch", password="Adm@stj2022")
+pyad.set_defaults(ldap_server="servidor",
+username="user", password="senha")
 
 emails = pd.Series(file_row['Email'])
 for raw in q.get_results():
